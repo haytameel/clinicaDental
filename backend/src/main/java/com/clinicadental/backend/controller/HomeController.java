78fb2c93ccdc5967a4bd6700594b526d788f3d52
haytameel <haytameelharhari@gmail.com>
2025-09-16 09:40:02 +0200

    Deployment, probandoo

--- backend/src/main/java/com/clinicadental/backend/controller/HomeController.java
+++ backend/src/main/java/com/clinicadental/backend/controller/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class HomeController {
 
-    @RequestMapping(value = {"/{path:[^\\.]*}", "/**/{path:[^\\.]*}"})
+    @RequestMapping(value = {"/{path:[^\\.]*}", "/{path:^(?!api$).*$}/**"})
     public String redirect() {
         // Renderizar siempre index.html para rutas del frontend
         return "forward:/index.html";
