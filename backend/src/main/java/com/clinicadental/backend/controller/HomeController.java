ab7f445f4f75752bf81c19c67e142921a8291157
haytameel <haytameelharhari@gmail.com>
2025-09-16 09:09:40 +0200

    probando deployment

--- backend/src/main/java/com/clinicadental/backend/controller/HomeController.java
+++ backend/src/main/java/com/clinicadental/backend/controller/HomeController.java
@@ -1,12 +1,25 @@
 package com.clinicadental.backend.controller;
 
+import com.clinicadental.backend.repository.PacienteRepository;
+import com.clinicadental.backend.service.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+    @Autowired
+    private PacienteService pacienteService;
+
     @GetMapping("/")
     public String home() {
         return "Backend funcionando 🚀";
+    }
+
+
+    @GetMapping("/prueba")
+    public String prueba() {
+        return pacienteService.getAllPacientes().toString();
     }
 }
